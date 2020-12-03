ccbb25f5db16f1e200141348888aadadd8d36564
LuanComputacao <eng.luanroger@gmail.com>
2020-12-03 10:25:33 -0300

    implementando templates com herança

---
+++ app/services/codewars_services.py
@@ -0,0 +1,15 @@
+def get_codewars_ranking():
+    return [
+        {
+            "name": "João Cardoso",
+            "points": 60
+        },
+        {
+            "name": "João Jesus",
+            "points": 90
+        },
+        {
+            "name": "John Doe",
+            "points": 30
+        }
+    ]
--- app/views/index_view.py
+++ app/views/index_view.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, render_template
 
+from app.services.codewars_services import get_codewars_ranking
+
 bp = Blueprint('index_route', __name__)
 
 
 @bp.route('/')
 def index():
+    codewars_ranking = get_codewars_ranking()
+
+    page_title = "Hanking Codewars"
+
     data = {
-        "resultado": 1 + 3,
-        "nome": "João Cardoso"
+        "codewars_ranking": codewars_ranking,
+        "page_title": page_title
     }
 
     return render_template('index.html', **data)
